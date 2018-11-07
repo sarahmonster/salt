54d0ee2cf8c7b513a04ff0fb57e398fa928ab2b5
sarah <sarah@triggersandsparks.com>
2018-11-07 00:20:29 +0000

    Add a super-simple frontpage.

---
+++ front-page.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying the front page.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Salt
+ */
+
+get_header(); ?>
+
+	<main id="primary" class="site-main">
+
+		<header class="page-header">
+			<h1 class="page-title">Hello, internet. <br>Let’s be friends.</h2>
+		</header>
+
+	</main><!-- #primary -->
+
+<?php
+get_footer();
