d4cd30189eed334b62d2292381e4165c2a77f336
sarah <sarah@triggersandsparks.com>
2018-12-24 12:23:41 +0000

    Clean up card layout for talks section.

--- template-parts/content-talk.php
+++ template-parts/content-talk.php
@@ -26,60 +26,62 @@
 
 	</header>
 
-	<?php // Output the name of the event.
-	if ( array_key_exists( 'link' , $meta ) ) :
-		printf( '<a href="%1$s" rel="bookmark">%2$s</a>',
-			esc_url( $meta['link'] ),
-			esc_html( get_the_title() )
-		);
-	else :
-		the_title();
-	endif;
-
-	// Format and output location.
-	if ( array_key_exists( 'location' , $meta ) && $meta['location'] ) :
-		echo '<br />';
-		echo $meta['location'];
-	endif;
-
-	// Format and output date.
-	if ( array_key_exists( 'date' , $meta ) && $meta['date'] ) :
-		echo '<br /> ';
-		if ( salt_date_is_upcoming( $meta['date'] ) ) :
-			echo date( 'l jS F Y', $meta['date'] );
+	<div class="salt-event-details">
+		<?php // Output the name of the event.
+		if ( array_key_exists( 'link' , $meta ) ) :
+			printf( '<a href="%1$s" rel="bookmark">%2$s</a>',
+				esc_url( $meta['link'] ),
+				esc_html( get_the_title() )
+			);
 		else :
-			echo date( 'F Y', $meta['date'] );
-		endif;
-	endif;
-
-	
-
-	if ( array_key_exists( 'slides' , $meta ) && $meta['slides'] OR array_key_exists( 'video' , $meta ) && $meta['video'] ) :
-		echo '<br />';
-
-		// Format and output link to slides.
-		if ( array_key_exists( 'slides' , $meta ) && $meta['slides'] ) :
-			printf( '<a href="%1$s" rel="bookmark">%2$s</a>',
-				esc_url( $meta['slides'] ),
-				esc_html__( 'Slides', 'salt' )
-			);
+			the_title();
 		endif;
 
-		if ( array_key_exists( 'slides' , $meta ) && $meta['slides']
-			&& array_key_exists( 'video' , $meta ) && $meta['video'] ) :
-			echo '|';
+		// Format and output location.
+		if ( array_key_exists( 'location' , $meta ) && $meta['location'] ) :
+			echo '<br />';
+			echo $meta['location'];
 		endif;
 
-		// Format and output link to video.
-		if ( array_key_exists( 'video' , $meta ) && $meta['video'] ) :
-			printf( '<a href="%1$s" rel="bookmark">%2$s</a>',
-				esc_url( $meta['video'] ),
-				esc_html__( 'Video', 'salt' )
-			);
+		// Format and output date.
+		if ( array_key_exists( 'date' , $meta ) && $meta['date'] ) :
+			echo '<br /> ';
+			if ( salt_date_is_upcoming( $meta['date'] ) ) :
+				echo date( 'l jS F Y', $meta['date'] );
+			else :
+				echo date( 'F Y', $meta['date'] );
+			endif;
 		endif;
 
-	endif;
+		
 
-	?>
+		if ( array_key_exists( 'slides' , $meta ) && $meta['slides'] OR array_key_exists( 'video' , $meta ) && $meta['video'] ) :
+			echo '<br />';
+
+			// Format and output link to slides.
+			if ( array_key_exists( 'slides' , $meta ) && $meta['slides'] ) :
+				printf( '<a href="%1$s" rel="bookmark">%2$s</a>',
+					esc_url( $meta['slides'] ),
+					esc_html__( 'Slides', 'salt' )
+				);
+			endif;
+
+			if ( array_key_exists( 'slides' , $meta ) && $meta['slides']
+				&& array_key_exists( 'video' , $meta ) && $meta['video'] ) :
+				echo '|';
+			endif;
+
+			// Format and output link to video.
+			if ( array_key_exists( 'video' , $meta ) && $meta['video'] ) :
+				printf( '<a href="%1$s" rel="bookmark">%2$s</a>',
+					esc_url( $meta['video'] ),
+					esc_html__( 'Video', 'salt' )
+				);
+			endif;
+
+		endif;
+
+		?>
+	</div>
 
 </article><!-- #post-## -->
