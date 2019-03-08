e1cbf16cb8fc1045603d520783c6c6050ed4e5bd
sarah <sarah@triggersandsparks.com>
2019-03-08 13:26:28 +0200

    Add robot talk infos.

---
+++ template-parts/talk-robots.php
@@ -0,0 +1,31 @@
+<p>How do you teach a computer to design a website?</p>
+
+<p>You may have heard that robots are coming to steal our jobs. But the end isn't nigh! I'm here to tell you why that may not be such a bad thing—and share some of my own adventures trying to automate away my own job.</p>
+
+<p>We’ll investigate how automation isn’t that different from other technological leaps of the past—as well as ways it is vastly different. We’ll touch on how emerging technologies can be an equaliser and the real-world application of machine learning. Along the way, we’ll talk about ethics, universal basic income, and how soon Judgement Day will be upon us. </p>
+
+<p>(Spoiler alert: I'm still employed!)</p>
+
+<!-- wp:heading -->
+<h2>Technological progress</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul><li><a href="https://gizmodo.com/42-visions-for-tomorrow-from-the-golden-age-of-futurism-1683553063">42 visions for tomorrow</a></li><li><a href="https://www.scientificamerican.com/article/is-there-a-future-for-airships/">Is there a future for airships?</a></li><li><a href="https://seniorplanet.org/days-of-future-passed/">Days of future passed</a></li><li><a href="https://www.businessinsider.com/novels-written-by-computers-2014-11?r=US&amp;IR=T">Novels <g class="gr_ gr_11 gr-alert gr_gramm gr_inline_cards gr_run_anim Grammar multiReplace" id="11" data-gr-id="11">written</g> by computers</a></li><li><a href="https://www.fastcompany.com/3058300/this-japanese-novel-authored-by-a-computer-is-scarily-well-written">This Japanese novel <g class="gr_ gr_12 gr-alert gr_gramm gr_inline_cards gr_run_anim Grammar multiReplace" id="12" data-gr-id="12">written</g> by a computer...</a></li><li><a href="https://www.theguardian.com/stage/2015/dec/01/beyond-the-fence-computer-generated-musical-greenham-common">World's first computer-generated musical to debut in London</a></li><li><a href="https://ourworldindata.org/technological-progress">Our World in Data: Technological progress</a></li></ul>
+<!-- /wp:list -->
+
+<!-- wp:heading -->
+<h2>Technological unemployment</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul><li><a href="http://hplusmagazine.com/2014/04/11/aristotle-robots-and-a-new-economic-system/">Aristotle, Robots, and a New Economic System</a></li><li><a href="https://www.mckinsey.com/featured-insights/digital-disruption/harnessing-automation-for-a-future-that-works">Harnessing automation for a future that works</a></li><li><a href="https://www.theguardian.com/inequality/datablog/2017/apr/26/inequality-index-where-are-the-worlds-most-unequal-countries">Inequality index</a></li><li><a href="https://willrobotstakemyjob.com">willrobotstakemyjob.com</a></li><li><a href="http://fortune.com/2016/07/11/skills-gap-automation/">These are the jobs least likely to go to robots</a></li><li><a href="https://futurism.com/finland-basic-income-trial">Finland's assessment of basic income trial</a></li><li><a href="https://www.singularityweblog.com/will-technological-unemployment-impoverish-us/">Will technological unemployment impoverish us?</a></li><li><a href="https://ourworldindata.org/working-hours">Our World in Data: Working hours</a></li><li><a href="http://www.spiegel.de/international/business/speed-of-innovation-and-automation-threatens-global-labor-market-a-897412.html">Are any jobs safe from automation?</a></li><li><a href="http://www.localhistories.org/unemployment.html">A history of unemployment</a></li></ul>
+<!-- /wp:list -->
+
+<!-- wp:heading -->
+<h2>AI and creative work</h2>
+<!-- /wp:heading -->
+
+<!-- wp:list -->
+<ul><li><a href="https://timelyapp.com/blog/the-future-of-ai-and-creativity/">The future of AI and creativity</a></li><li><a href="https://algorithms.design/">Algorithm-driven design</a></li></ul>
+<!-- /wp:list -->
